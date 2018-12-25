bdbbc28ac8ad0ddb41136854688fcaa0e9c5d4b5
bwjustus <bwjustus@berkeley.edu>
2018-12-24 20:07:43 -0800

    ch06 code

--- week1/ch06/table_of_contents.rb
+++ week1/ch06/table_of_contents.rb
@@ -1,4 +1,14 @@
 # Print out the table of contents for Learn to Program in the form shown in the README.
 
 
-### Your Code Here ###
+space = 75
+puts 'Table of Contents'.center(space)
+puts 'Chapter 1: Getting Started'.ljust(space/2)      +      'page 1'.rjust(space/2)
+puts 'Chapter 2: Numbers'.ljust(space/2)           +         'page 9'.rjust(space/2)
+puts 'Chapter 3: Letters'.ljust(space/2)          +         'page 13'.rjust(space/2)
+puts 'Chapter 4: Variables and Assignment'.ljust(space/2) + 'page 17'.rjust(space/2)
+puts 'Chapter 5: Mixing It Up'.ljust(space/2)       +       'page 21'.rjust(space/2)
+puts 'Chapter 6: More About Methods'.ljust(space/2)    +    'page 27'.rjust(space/2)
+puts 'Chapter 7: Flow Control'.ljust(space/2)       +       'page 37'.rjust(space/2)
+puts 'Chapter 8: Arrays and Iterators'.ljust(space/2)   +   'page 51'.rjust(space/2)
+puts 'Chapter 9: Writing Your Own Methods'.ljust(space/2) + 'page 57'.rjust(space/2)
