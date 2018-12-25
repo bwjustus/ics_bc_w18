6aed7ad433667fd533ab97018c493f8284c54343
bwjustus <bwjustus@berkeley.edu>
2018-12-24 19:35:35 -0800

    ch05 code

--- week1/ch05/bigger_better_favorite_number.rb
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -3,4 +3,7 @@
 #  - Suggest their favorite number plus one as a better option
 
 
-### Your Code Here ###
+puts 'Hello! What is your favorite number?'
+num = gets.chomp
+num1 = num.to_i + 1
+puts num + ' is a good favorite number, but may I suggest ' + num1.to_s + ' as an alternative?'
