9319f0819f6a3ea8d230ba9621b418f682839c58
bwjustus <bwjustus@berkeley.edu>
2019-01-16 15:48:01 -0800

    week 4

--- week4/recursion_practice/array_max.rb
+++ week4/recursion_practice/array_max.rb
@@ -1,18 +1,17 @@
 # Assume a non-empty array of integers
 # Don't forget to use array indexing and array.pop() to remove the last element of an array
 
-def array_max(array)
+def array_max array
   if array.length == 1  # base case
-    return _________
+    return array[0]
   else
-    last = _________
-    rest = _________________  # recursive call - array is now shorter
+    last = array[-1]
+    rest = array_max array[0..-2]# recursive call - array is now shorter
     if last > rest
-      return ______
+      return last
     else
-      return ______
+      return rest
     end
   end
 end
-
 puts(array_max([3, 5, -2, 8, 4])) # expected: 8
--- week4/recursion_practice/count_array.rb
+++ week4/recursion_practice/count_array.rb
@@ -1,13 +1,16 @@
 # If you are stuck, take a look at the pseudo code for file/folder example
 # Use .kind_of?(Array) method
-
 def count_array(array)
   count = 0
-
-
-  # FILL IN WITH YOUR CODE #
-
-
+  array.each do |sub|
+    if sub.kind_of?(Array)
+      count += count_array(sub)
+    else
+      count += 1
+    end
+  end
+  return count
 end
+
 
 puts(count_array([[2, 4, 6], 8, [10, 12, [14, 16]]])) # expected: 8
